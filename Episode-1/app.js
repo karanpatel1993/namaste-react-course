d8ddb9261a0e8487815bd7d98fc69de0d97879cc
Karan Patel <k.patel1121993@gmail.com>
2024-01-20 21:27:43 +0530

    Episode-1 of namaste-react

---
+++ Episode-1/app.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hi, I am a h1 element"),
+    React.createElement("h2", {}, "Hi, I am a h2 element"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hi, I am a h1 element"),
+    React.createElement("h2", {}, "Hi, I am a h2 element"),
+  ]),
+]);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
